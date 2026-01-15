77075d83f33b20a76700dd8e0a5654c2d76f6348
asifahmed1467 <rockyasif825@gmail.com>
2026-01-15 14:29:41 +0600

    home control php

---
+++ PROJECT/Management/User/MVC/php/home_controller.php
@@ -0,0 +1,9 @@
+<?php
+// Management/User/MVC/php/home_controller.php
+
+// In MVC, the controller initiates the session if needed and loads the view
+session_start();
+
+// Load the HTML View file
+include "../html/home_view.php";
+?>
